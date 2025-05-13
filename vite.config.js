0c0078d90346e39d37fc3ba7e88e9c1f2d7aa6a3
jolie <jolieheejikim@gmail.com>
2025-05-14 01:28:57 +0900

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 3001
-  }
+  base: './',
 })
